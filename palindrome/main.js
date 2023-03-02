66f48706a34624858a817ef8a0a42ef6f7133373
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-02 16:50:42 +0100

    debug function name

--- palindrome/main.js
+++ palindrome/main.js
@@ -1,23 +1,23 @@
 // program to check if the string is palindrome or not
-function checkPalindrome(string) {
+function checkPalindrome(stringer) {
 
 // convert string to an array so the word became a sum of single letters
-const arrayValues = string.split('');
+const arrayValues = stringer.split('');
 // reverse the array values...reverse the letter's direction
 const reverseArrayValues = arrayValues.reverse();
 // convert array to string so transform the revers-array in a new string
 const reverseString = reverseArrayValues.join('');
-// display the cotent inside the <p> element
+// display the content inside the <p> element
 let palindromeDisplay = document.getElementById('palindrome')
 
 // if function to check if is a palindrome: 'string' and 'reversString' are two string
-  if(string == reverseString) {
+  if(stringer == reverseString) {
     palindromeDisplay.textContent = `It is a palindrome`;
   }
   else {
     palindromeDisplay.textContent = `It is not a palindrome`;
   }
-  
+
 };
 
 //take input from the prompt
